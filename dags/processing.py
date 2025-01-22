488a7ea79a8265d7c03b09c37d7f8fb0e6613e5d
oleg <oleg.sharnikov@quandoo.com>
2025-01-22 14:17:43 +0100

    Add 3 data imports

--- dags/processing.py
+++ dags/processing.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 
 default_args = {
@@ -27,4 +28,28 @@ with DAG(
         dag=dag,
     )
 
-    steps_import
+    patients_import = SparkSubmitOperator(
+        task_id="patients_import",
+        application="/usr/local/airflow/include/runner.py",
+        application_args=["-s", "patients"],
+        jars='/usr/local/airflow/include/jars/hadoop-aws-3.3.4.jar,/usr/local/airflow/include/jars/aws-java-sdk-bundle-1.12.779.jar,/usr/local/airflow/include/jars/postgresql-42.7.0.jar',
+        name="patients_import_job",
+        conn_id="sparkon",
+        verbose=True,
+        dag=dag,
+    )
+
+    exercises_import = SparkSubmitOperator(
+        task_id="exercises_import",
+        application="/usr/local/airflow/include/runner.py",
+        application_args=["-s", "exercises"],
+        jars='/usr/local/airflow/include/jars/hadoop-aws-3.3.4.jar,/usr/local/airflow/include/jars/aws-java-sdk-bundle-1.12.779.jar,/usr/local/airflow/include/jars/postgresql-42.7.0.jar',
+        name="exercises_import_job",
+        conn_id="sparkon",
+        verbose=True,
+        dag=dag,
+    )
+
+    end = DummyOperator(task_id='end')
+
+    [steps_import, patients_import, exercises_import] >> end
