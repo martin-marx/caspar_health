2cf87cd68d03c27f94016530613be68552f7039d
oleg <oleg.sharnikov@quandoo.com>
2025-01-23 16:28:36 +0100

    Refactor a bit

--- components/loader/runner.py
+++ components/loader/runner.py
@@ -2,21 +2,28 @@ import argparse
 from schema.schema_enum import SchemaEnum
 from pyspark.sql import SparkSession
 
-if __name__ == '__main__':
-
+def parse_args():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-s', '--source', type=str, help='Source of the data', required=True)
+    parser.add_argument('-sh', '--source_host', type=str, help='Source host', default="http://minio:9000")
+    parser.add_argument('-sih', '--sink_host', type=str, help='Sink host', default="dwh4")
+    parser.add_argument('-sip', '--sink_port', type=int, help='Sink port', default=5432)
+    parser.add_argument('-sid', '--sink_db', type=str, help='Sink DB', default="main")
 
-    source_arg = parser.parse_args().source
+    return parser.parse_args()
 
-    spark_schema = SchemaEnum.get_schema(source_arg)
-    file_path = SchemaEnum.get_file_path(source_arg)
+if __name__ == '__main__':
+
+    parsed_args = parse_args()
+
+    spark_schema = SchemaEnum.get_schema(parsed_args.source)
+    file_path = SchemaEnum.get_file_path(parsed_args.source)
 
     spark = (SparkSession.builder
              .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
              # Next line can be used during local runs
              #.config("spark.jars", "./jars/hadoop-aws-3.3.4.jar,./jars/aws-java-sdk-bundle-1.12.779.jar, ./jars/postgresql-42.7.0.jar")
-             .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
+             .config("spark.hadoop.fs.s3a.endpoint", parsed_args.source_host)
              .config("spark.hadoop.fs.s3a.access.key", "datauser")
              .config("spark.hadoop.fs.s3a.secret.key", "password")
              .config("spark.hadoop.fs.s3a.path.style.access", "true")
@@ -27,9 +34,9 @@ if __name__ == '__main__':
 
     (df
      .write.format("jdbc")
-     .option("url", "jdbc:postgresql://dwh4:5432/main")
+     .option("url", f"jdbc:postgresql://{parsed_args.sink_host}:{parsed_args.sink_port}/{parsed_args.sink_db}")
      .option("driver", "org.postgresql.Driver")
-     .option("dbtable", source_arg)
+     .option("dbtable", parsed_args.source)
      .option("user", "datauser")
      .option("password", "password")
      .mode("append")
