cf2306ef0097f9ec52f4b94eec4cb5d3e1a2a3f7
oleg <oleg.sharnikov@quandoo.com>
2025-01-23 17:04:21 +0100

    Refactor a bit

--- components/loader/runner.py
+++ components/loader/runner.py
@@ -1,11 +1,14 @@
 import argparse
+import os
+
 from schema.schema_enum import SchemaEnum
 from pyspark.sql import SparkSession
 
 def parse_args():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-s', '--source', type=str, help='Source of the data', required=True)
-    parser.add_argument('-sh', '--source_host', type=str, help='Source host', default="http://minio:9000")
+    parser.add_argument('-sh', '--source_host', type=str, help='Source host', default="http://minio")
+    parser.add_argument('-sp', '--source_port', type=int, help='Source port', default=9000)
     parser.add_argument('-sih', '--sink_host', type=str, help='Sink host', default="dwh4")
     parser.add_argument('-sip', '--sink_port', type=int, help='Sink port', default=5432)
     parser.add_argument('-sid', '--sink_db', type=str, help='Sink DB', default="main")
@@ -23,9 +26,9 @@ if __name__ == '__main__':
              .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
              # Next line can be used during local runs
              #.config("spark.jars", "./jars/hadoop-aws-3.3.4.jar,./jars/aws-java-sdk-bundle-1.12.779.jar, ./jars/postgresql-42.7.0.jar")
-             .config("spark.hadoop.fs.s3a.endpoint", parsed_args.source_host)
-             .config("spark.hadoop.fs.s3a.access.key", "datauser")
-             .config("spark.hadoop.fs.s3a.secret.key", "password")
+             .config("spark.hadoop.fs.s3a.endpoint", f"{parsed_args.source_host}:{parsed_args.source_port}")
+             .config("spark.hadoop.fs.s3a.access.key", os.getenv("MINIO_ROOT_USER"))
+             .config("spark.hadoop.fs.s3a.secret.key", os.getenv("MINIO_ROOT_PASSWORD"))
              .config("spark.hadoop.fs.s3a.path.style.access", "true")
              .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
              .appName('Data importer').getOrCreate())
@@ -37,7 +40,7 @@ if __name__ == '__main__':
      .option("url", f"jdbc:postgresql://{parsed_args.sink_host}:{parsed_args.sink_port}/{parsed_args.sink_db}")
      .option("driver", "org.postgresql.Driver")
      .option("dbtable", parsed_args.source)
-     .option("user", "datauser")
-     .option("password", "password")
+     .option("user", os.getenv("POSTGRES_USER"))
+     .option("password", os.getenv("POSTGRES_PASSWORD"))
      .mode("append")
      .save())
